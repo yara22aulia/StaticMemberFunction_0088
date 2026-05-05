fb6d68ae4efd262ae60926eb087cf6aea4cb4c58
yara22aulia <yara22aulia@gmail.com>
2026-05-05 17:46:37 +0700

    menggunakan keyword this untuk mengakses member class

---
+++ this.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class buku {
+    string judul;
+
+public:
+    string setget(string judul) {
+        this->judul = judul; // nilai parameter 'judul' untuk member variabel 'judul'
+        return this->judul;  // return variabel judul.
+    }
+};
+
+int main() 
+{
+    buku bukunya;
+    cout << bukunya.setget("Matematika");
+    return 0;
+}
