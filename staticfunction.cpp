57ff7c34abb30be2d5a754d084ed362f1e50985c
yara22aulia <yara22aulia@gmail.com>
2026-05-05 17:26:34 +0700

    menambahkan fungsi static setter dan getter nim

---
+++ staticfunction.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+class Mahasiswa 
+{
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    // pembuatan static function
+    static void setNim(int pnim) {
+        nim = pnim;
+    };
+
+    static int getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) : nama(pnama) {
+        setID();
+    };
+};
+
+// Inisialisasi static member di luar class
+int Mahasiswa::nim = 0;
+
+// Pendefinisian prosedur di luar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+    // pembuatan object dan pemberian nilai
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+
+    // memberi nilai pada setNim() untuk merubah nilai NIM secara static
+    Mahasiswa::setNim(9);
+
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
+
+    // memanggil prosedur printAll()
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    // menampilkan NIM terakhir yang diproses
+    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
+    
+    system("pause");
+    return 0;
+}
