96cded0a98b562511f6096d7700e9ea574e9ec9c
yara22aulia <yara22aulia@gmail.com>
2026-05-05 17:58:58 +0700

    mengimplementasikan chain function pada class buku

---
+++ chainfunctioncall.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class buku {
+    string judul;
+
+public:
+    buku &setJudul(string judul) {
+        this->judul = judul;
+        return *this;   // chain function
+    }
+
+    string getJudul() {
+        return this->judul;
+    }
+};
+
+int main()
+{
+    buku bukunya;
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul() << endl;
+
+    cout << bukunya.setJudul("Matematika").getJudul();   // chain function calls
+
+    return 0;
+}
